fb453d838c0083ef982f069d4745e4a7217976c0
Harmedino <88965795+Harmedino@users.noreply.github.com>
2021-09-29 07:33:04 -0700

    Add files via upload

---
+++ begin/css/cal.js
@@ -0,0 +1,23 @@
+let runningTotal = 0;
+let buffer = "0";
+let previousOperator;
+
+const screen = document.querySelector('.screen');
+
+function buttonClick(value) {
+    if  (isNaN(value) )
+} else
+
+function handleSymbol(){}
+function handleNumber(){}
+
+function init () {
+    document.querySelector('.cal-button') 
+    .addEventListener('click' , function(event) {
+    
+        buttonClick
+        (event.target.innerText);
+    })
+}
+
+init();
